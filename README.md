e43804640ffea4875aaa0887d006d7be9ecab7e6
omiguelgomes <miguelrgomes1698@gmail.com>
2020-04-02 23:12:53 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# GameOfLife
-Simple implementation of the famous game of life using pygame
+Como iniciar o jogo:
+
+O projeto é feito na linguagem Python. As instruções seguintes assumem que o programa é corrido em Linux.
+Para inicial os seguintes comandos devem ser executados na pasta que contém os ficheiros .py
+```
+sudo apt install python-pip
+```
+
+Os proximos dois comandos garantem que as bibliotecas essenciais estão instaladas:
+```
+pip install pygame
+```
+
+```
+pip install numpy
+```
+
+Para executar o programa:
+```
+python main.py
+```
---
+++ utils.py
@@ -0,0 +1,61 @@
+import copy
+rowNr = 31
+colNr = 38
+
+def init(pygame): 
+    pygame.init()
+    pygame.display.set_caption("Conway's Game Of Life")
+    win = pygame.display.set_mode((1800, 1800))
+    
+    return win
+
+#Recieves list of key presses to check for
+def checkInputs(pygame, options):
+    # check mouse events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            return pygame.mouse.get_pos()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key in options:
+                return event.key
+
+def isValidPos(row, col):
+    return row >= 0 and row < rowNr and col >= 0 and col < colNr
+
+def neighbourSum(grid, row, col):
+    sum = 0
+    if grid[row][col] == 1:
+        sum -= 1
+
+    for deltaRow in range(-1, 2):
+        for deltaCol in range(-1, 2):
+            if isValidPos(row + deltaRow, col + deltaCol):
+                sum += grid[row + deltaRow][col + deltaCol]
+    return sum
+
+
+def nextStatus(grid, row, col):
+    sum = neighbourSum(grid, row, col)
+
+    if grid[row][col] == 1:
+        if sum == 2 or sum == 3:
+            return 1
+        else:
+            return 0
+    if grid[row][col] == 0:
+        if sum == 3:
+            return 1
+        else:
+            return 0
+ 
+def updateGrid(grid):
+    newGrid = copy.deepcopy(grid)
+    for row in range (0, rowNr):
+        for col in range(0, colNr):
+            newGrid[row][col] = nextStatus(grid, row, col)
+    return newGrid
